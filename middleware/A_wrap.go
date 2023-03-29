6c9048fa024bc1edbff9c05b6f33d5b5314612a3
김준호 <kjunho040622@gmail.com>
2023-03-29 11:33:39 +0900

    ^

---
+++ middleware/A_wrap.go
@@ -0,0 +1,64 @@
+package middleware
+
+import (
+	"bytes"
+	"gin-login/internal/constants"
+	"gin-login/pkg/cerror"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"runtime/debug"
+	"strings"
+)
+
+func WrapMiddleware(c *gin.Context) {
+	if !strings.Contains(c.Request.URL.Path, "swagger") && !strings.Contains(c.Request.URL.Path, "static") {
+		c.Writer.Header().Set("Content-Type", "application/json")
+	}
+	c.Writer.Header().Set(constants.HeaderCacheControl, constants.CacheNoStore)
+
+	bodyByte, _ := io.ReadAll(c.Request.Body)
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyByte))
+
+	defer func() {
+
+		//error handling
+
+		//r 로 panic 이 일어나서 recover 를 일으켰는지 확인하고
+		//panic 이 일어났다면 아래 switch 문을 실행
+		if r := recover(); r != nil {
+			var customError cerror.CustomError
+			switch r.(type) {
+			case error:
+				customError = cerror.CustomError{
+					StatusCode: http.StatusInternalServerError,
+					Message:    http.StatusText(http.StatusInternalServerError),
+				}
+			case string:
+				customError = cerror.CustomError{
+					StatusCode: http.StatusInternalServerError,
+					Message:    http.StatusText(http.StatusInternalServerError),
+				}
+			case cerror.CustomError:
+				customError = r.(cerror.CustomError)
+			default:
+				customError = cerror.CustomError{
+					StatusCode: http.StatusInternalServerError,
+					Message:    http.StatusText(http.StatusInternalServerError),
+				}
+			}
+			c.JSON(customError.StatusCode, customError)
+
+			errorStack := string(debug.Stack())
+			log.Println(errorStack)
+			log.Println(customError)
+
+			//원래는 이거 안썼었음
+			c.Abort()
+
+		}
+	}()
+
+	c.Next()
+}
